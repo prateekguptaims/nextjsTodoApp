2fd380d8b514aeba1ec66a97657f7deec512cca7
prateekguptaims <prateekkumargupta45@gmail.com>
2023-05-09 21:07:15 +0530

    meta

--- Pages/api/auth/me.js
+++ Pages/api/auth/me.js
@@ -1,23 +1,19 @@
-import { checkAuth} from "@/utils/features";
+import { asyncError } from "@/middlewares/error";
+import { errorHandler } from "@/middlewares/error";
+import { checkAuth } from "@/utils/features";
 
-const { asyncError, errorHandler } = require("@/middlewares/error");
+const handler = asyncError(async (req, res) => {
+  if (req.method !== "GET")
+    return errorHandler(res, 400, "Only GET Method is allowed");
 
+  const user = await checkAuth(req);
 
-const handler=asyncError(async(req,res)=>{
-    if(req.method !=="GET")
-        return errorHandler(res,400,"only get method allowed");
+  if (!user) return errorHandler(res, 401, "Login First");
 
-        const user=await checkAuth(req); 
-        if(!user) return errorHandler(res,401,"Login first");
-
-
-res.status(200).json({
-    success:true,
+  res.status(200).json({
+    success: true,
     user,
-    
-});
-
-
+  })
 });
 
 export default handler;
